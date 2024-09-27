09310a11c7e41bc82c515f5f27f56907b9ae4df9
LucasSC98 <luccas.custtodio98@gmail.com>
2024-09-27 13:02:47 -0300

    Comando do inventario e help

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -4,66 +4,90 @@ document.addEventListener('DOMContentLoaded', function () {
     const output = document.getElementById('output');
     const helpTexto = document.getElementById('helpTexto');
     const inventarioLista = document.getElementById('inventario-lista');
+    const inventario = document.getElementById('inventory');
+    const ajuda = document.getElementById('help')
 
 
-    addMessageToOutput("O mundo enfrenta uma invasão de mortos-vivos, que começou há duas semanas e tem piorado. Após um ataque quase fatal, eu me abriguei em uma casa abandonada para descansar. No entanto, uma horda de zumbis se aproxima e preciso proteger o local antes do pôr do sol para tentar sobreviver mais um dia. Digite \"start\" para começar o jogo.");
+    // addMessageToOutput("O mundo enfrenta uma invasão de mortos-vivos, que começou há duas semanas e tem piorado. Após um ataque quase fatal, eu me abriguei em uma casa abandonada para descansar. No entanto, uma horda de zumbis se aproxima e preciso proteger o local antes do pôr do sol para tentar sobreviver mais um dia. Digite \"start\" para começar o jogo.");
+    addMessageToOutput("Start - Começar jogo <br> Load - Carregar Save <br>");
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
-        const userInput = input.value.trim();
+        const userInput = input.value.trim().toLowerCase();
         if (userInput === '') return;
 
         addMessageToOutput(`Jogador: ${userInput}`);
-
-        fetch('/game', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: new URLSearchParams({ input: userInput })
-        })
-            .then(response => response.json())
-            .then(data => {
-                addMessageToOutput(`Jogo: ${data.message}`);
-                mostrarHelp(data.helpTexto);
-                atualizarInventario(data.inventario);
-                mostrarCenaLocal(data.local); // Cena nome
-                input.value = ''; // Limpar campo
+        if (userInput === 'inventory') {
+            inventario.style.display = 'block';
+        } else if (userInput === 'close inventory') {
+            inventario.style.display = 'none';
+        } else if (userInput === 'help') {
+            ajuda.style.display = 'block';
+        } else if (userInput === 'close help') {
+            ajuda.style.display = 'none'
+        } else {
+            fetch('/game', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: new URLSearchParams({input: userInput})
             })
-            .catch(error => {
-                console.error('Erro:', error);
-                addMessageToOutput('Erro ao enviar comando.');
-            });
+                .then(response => response.json())
+                .then(data => {
+                    addMessageToOutput(`Jogo: ${data.message}`);
+                    mostrarHelp(data.helpTexto);
+                    atualizarInventario(data.inventario);
+                    mostrarCenaLocal(data.local); // Cena nome
+                    input.value = ''; // Limpar campo
+                })
+                .catch(error => {
+                    console.error('Erro:', error);
+                    addMessageToOutput('Comando Invalido, tente outro');
+                });
+        }
     });
 
-    function addMessageToOutput(message) {
+    async function addMessageToOutput(message) {
         const messageElement = document.createElement('div');
-        messageElement.textContent = message;
         output.appendChild(messageElement);
         output.scrollTop = output.scrollHeight;
-    }
 
+        const parts = message.split('<br>').map(part => part.trim());
+
+        for (const part of parts) {
+            if (part) {
+                for (const char of part) {
+                    messageElement.innerHTML += char;
+                    await new Promise(resolve => setTimeout(resolve, 3)); // 3 ms cada caractere
+                }
+                messageElement.innerHTML += '<br>'; //Adiciona quebra de linha
+            }
+        }
+        output.scrollTop = output.scrollHeight; // Scroll para o final após escrever a msg
+    }
 
     function mostrarCenaLocal(meuLocal) {
         const localizacao = document.getElementById('local');
         localizacao.textContent = `Cena Local: ${meuLocal}`;
     }
 
-    function mostrarHelp(helpText){
+    function mostrarHelp(helpText) {
         helpTexto.innerHTML = '';
-        const comandoSeparado = helpText.split(','); // Separar os comando, quando tiver virgula
-        comandoSeparado.forEach(comandoSeparado =>{
-            const comandoElement = document.createElement('div')
-            comandoElement.textContent= comandoSeparado.trim();
+        const comandoSeparado = helpText.split(','); // Separar os comando, quando tiver vírgula
+        comandoSeparado.forEach(comando => {
+            const comandoElement = document.createElement('div');
+            comandoElement.textContent = comando.trim().toUpperCase();
             helpTexto.appendChild(comandoElement);
-        })
+        });
     }
-    function atualizarInventario(itens){
+
+    function atualizarInventario(itens) {
         inventarioLista.innerHTML = '';
         itens.forEach(item => {
             const listaItens = document.createElement('li');
             listaItens.textContent = `${item.nome} (Quantidade: ${item.quantidade})`;
             inventarioLista.appendChild(listaItens);
-        })
+        });
     }
 });
